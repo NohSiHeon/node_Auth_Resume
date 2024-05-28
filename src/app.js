7a076dcd74a7868e67ccd4806d16fae98df52a56
NohSiHeon <shtlgjs016@naver.com>
2024-05-29 02:23:39 +0900

    chore: DB연결, 스키마 작성, 테이블 생성

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+// import './utils/prisma.util.js';
+import { prisma } from './utils/prisma.util.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
 
 app.listen(PORT, () => {
 	console.log(PORT, '포트로 서버가 열렸어요');
